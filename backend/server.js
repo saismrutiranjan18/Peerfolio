d98196df977231f6ef59f3b1f035b823e98f9378
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-10-28 00:59:23 +0530

    second commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import postRoutes from "./Routes/PostRoute.js";
+import userRoutes from "./Routes/UserRoute.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.use(postRoutes);
+app.use(userRoutes);
+
+const URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 8000;
+
+main()
+  .then(() => {
+    console.log("Connection Successful!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(URL);
+}
+
+app.get("/test", (req, res) => {
+  res.json("All set");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port : ${PORT}`);
+});
